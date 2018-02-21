3a3e20771151597fabca2fc539abc9c2cc1f2a44
Christian Semmler <mail@csemmler.com>
2018-02-21 16:13:25 +0100

    allow overwriting compilers

--- ext/rj_schema/extconf.rb
+++ ext/rj_schema/extconf.rb
@@ -1,4 +1,6 @@
 require 'mkmf'
 $INCFLAGS << " -I$(srcdir)/rapidjson/include"
 $CXXFLAGS << " -std=c++11 "
+CONFIG['CXX'] = ENV['CXX'] unless ENV['CXX'].nil?
+CONFIG['CC'] = ENV['CC'] unless ENV['CC'].nil?
 create_makefile 'rj_schema/rj_schema'
