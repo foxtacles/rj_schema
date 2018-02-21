a8d25310fa7850db304cd0c57e0e107e5e4ab842
Christian Semmler <mail@csemmler.com>
2018-02-21 16:23:45 +0100

    add debugging puts

--- ext/rj_schema/extconf.rb
+++ ext/rj_schema/extconf.rb
@@ -3,4 +3,5 @@ $INCFLAGS << " -I$(srcdir)/rapidjson/include"
 $CXXFLAGS << " -std=c++11 "
 CONFIG['CXX'] = ENV['CXX'] unless ENV['CXX'].nil?
 CONFIG['CC'] = ENV['CC'] unless ENV['CC'].nil?
+puts "compiler ENV: CC #{ENV['CC']}, CXX #{ENV['CXX']}"
 create_makefile 'rj_schema/rj_schema'
