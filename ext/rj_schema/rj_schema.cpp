815fbb42757af958dd68545b9c89c9dd8a77135e
Christian Semmler <mail@csemmler.com>
2020-03-26 15:02:59 +0100

    fix for OSX

--- ext/rj_schema/rj_schema.cpp
+++ ext/rj_schema/rj_schema.cpp
@@ -12,7 +12,7 @@
 #include <ruby.h>
 #include <ruby/version.h>
 
-#if RUBY_API_VERSION_MAJOR > 2 || (RUBY_API_VERSION_MAJOR == 2 && RUBY_API_VERSION_MINOR >= 7)
+#if (RUBY_API_VERSION_MAJOR > 2 || (RUBY_API_VERSION_MAJOR == 2 && RUBY_API_VERSION_MINOR >= 7)) && defined(__GLIBC__)
 namespace std {
 	static inline void* ruby_nonempty_memcpy(void *dest, const void *src, size_t n) {
 		return ::ruby_nonempty_memcpy(dest, src, n);
